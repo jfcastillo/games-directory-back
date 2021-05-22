680ef86abc937972e12c57c16daa126467599c54
Sergio <sergiolozada124@gmail.com>
2021-05-21 23:15:57 -0500

    initial commit

---
+++ src/main/java/com/games/directory/GamesDirectoryBack.java
@@ -0,0 +1,13 @@
+package com.games.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesDirectoryBack {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesDirectoryBack.class, args);
+	}
+
+}
